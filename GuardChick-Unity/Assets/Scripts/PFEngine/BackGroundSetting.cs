8efc002277c58608ecba95eb8ad51f465f381d93
KerryYang <yyy98yangwei@sina.com>
2015-10-22 15:08:57 +0800

    Setup project!

---
+++ GuardChick-Unity/Assets/Scripts/PFEngine/BackGroundSetting.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundSetting : MonoBehaviour {
+	public float ratioX = 1.0f;
+	public float ratioY = 1.0f;
+}
---
+++ GuardChick-Unity/Assets/Scripts/PFEngine/InfiniteScrollBG.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfiniteScrollBG : MonoBehaviour {
+	private Vector2 materialOffsetVect2;
+	private Material backgroundMatrial;
+	private BackGroundSetting setting;
+
+	void Start () {
+		materialOffsetVect2 = new Vector2 ();
+	}
+
+	public void SetBackgroundMaterial(Material bgMaterial) {
+		backgroundMatrial = bgMaterial;
+	}
+
+	public void SetBackgroundSetting(BackGroundSetting bgSetting) {
+		setting = bgSetting;
+	}
+
+	public void ScrollBG(float offsetX, float offsetY) {
+		if (backgroundMatrial != null && setting != null) {
+			materialOffsetVect2.x = Mathf.Repeat (materialOffsetVect2.x + offsetX * setting.ratioX, 1.0f);
+			materialOffsetVect2.y = Mathf.Repeat (materialOffsetVect2.y + offsetY * setting.ratioY, 1.0f);
+			backgroundMatrial.SetTextureOffset ("_MainTex", materialOffsetVect2);
+		} else {
+			Debug.Log("Warn! : Background matrial or setting is null!");
+		}
+	}
+	
+	void OnDisable () {
+		if (backgroundMatrial != null) {
+			backgroundMatrial.SetTextureOffset ("_MainTex", Vector2.zero);
+		}
+	}
+
+}
