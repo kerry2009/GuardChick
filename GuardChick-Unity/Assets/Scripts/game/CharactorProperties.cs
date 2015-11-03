3851661f89724aefe348b8b07d9cb65942874b1d
wyang@pourfun.com <wyang@pourfun.com>
2015-11-03 21:29:05 +0800

    GuardChick-Unity/Assets/Scripts/game/CharactorProperties.cs   GuardChick-Unity/Assets/Scripts/game/HeroController.cs   GuardChick-Unity/Assets/Scripts/game/HeroProperties.cs

---
+++ GuardChick-Unity/Assets/Scripts/game/CharactorProperties.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharactorProperties {
+	public float hp;
+}
---
+++ GuardChick-Unity/Assets/Scripts/game/HeroController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour {
+	public SkeletonAnimation avatar;
+	public BoxCollider2D attackArea;
+
+	private int CURRENT_STATE = 0;
+	private HeroProperties properties;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
